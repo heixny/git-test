2e17fef29650617b477015999595239d074ba665
heix <heixlie@0101.com>
2023-03-06 14:27:47 +0800

    master test

--- src/main/java/com/gec/git/GitTest.java
+++ src/main/java/com/gec/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git!!! 2222");
         System.out.println("hello git!!! 33");
         System.out.println("hello git!!! hot-fix");
+        System.out.println("master test");
     }
 }
